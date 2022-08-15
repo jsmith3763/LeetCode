02008af25fd0d9093519b6e939f106a0fd2dbfa7
jsmith3763 <57576309+jsmith3763@users.noreply.github.com>
2022-08-14 17:37:09 -0700

    Time: 129 ms (48.31%), Space: 43.6 MB (19.36%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    let returnnums = [];
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let currentNum = nums[i];
+        let targetNum = target - nums[i];
+        if (map.has(targetNum)) {
+            returnnums.push(map.get(targetNum));
+            returnnums.push(i);
+            return returnnums;
+        }
+        map.set(currentNum, i);
+        // console.log(map);
+    }
+    return false;
+}
