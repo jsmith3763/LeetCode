8cc88111029e6de9f173b252f8bd2babb804b40d
jsmith3763 <57576309+jsmith3763@users.noreply.github.com>
2022-09-15 12:13:50 -0700

    Time: 112 ms (52.89%), Space: 43.7 MB (97.27%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -6,12 +6,11 @@ const isPalindrome = function(string) {
   string = string.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
   let leftPointer = 0, rightPointer = string.length - 1;
   while (leftPointer < rightPointer) {
-    if (string[leftPointer] != string[rightPointer]) {
+    if (string[leftPointer] !== string[rightPointer]) {
       return false;
-    } else {
-      leftPointer++
-      rightPointer--
-    }
+    } 
+    leftPointer++
+    rightPointer--
   }
   return true;
 }
