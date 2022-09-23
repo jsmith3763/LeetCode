8dec65d20a271c4f6ceb7b691afd39d5804d6d1f
jsmith3763 <57576309+jsmith3763@users.noreply.github.com>
2022-09-23 11:28:12 -0700

    Time: 124 ms (44.77%), Space: 42.9 MB (95.04%) - LeetHub

---
+++ 136-single-number/136-single-number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    if(nums.length === 1) {
+        return nums[0]
+    }
+    
+    let result = 0;
+    
+    nums.forEach(element => {
+        result = result ^ element
+    })
+    
+    return result
+    
+    
+};
