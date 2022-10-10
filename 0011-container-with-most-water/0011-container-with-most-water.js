5ee4446c423fe870366176e4b38762c848cef0fc
jsmith3763 <57576309+jsmith3763@users.noreply.github.com>
2022-10-10 14:11:26 -0700

    Time: 137 ms (32.48%), Space: 49.7 MB (56.79%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+const maxArea = (arr) => {
+  let maxArea = 0
+  let p1 = 0
+  let p2 = arr.length - 1
+  while(p1 < p2) {
+    let height = Math.min(arr[p1], arr[p2])
+    let width = p2 - p1
+    let area = height * width
+    maxArea = Math.max(maxArea, area)
+
+    if(arr[p1] <= arr[p2]) {
+      p1++
+    }else {
+      p2--
+    }
+  }
+  return maxArea
+}
