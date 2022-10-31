752676c009b9b390680936152338278a7643d470
jsmith3763 <57576309+jsmith3763@users.noreply.github.com>
2022-10-31 09:26:17 -0700

    Time: 73 ms (99.33%), Space: 46.3 MB (40.34%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+let returnArr=[];
+for(let i = 0; i < nums.length; i++){
+returnArr.push(nums[nums[i]]);
+
+};
+
+return returnArr;
+};
